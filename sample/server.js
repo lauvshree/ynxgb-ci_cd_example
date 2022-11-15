382415ad861925974b91c056abc664442de01790
Lavanya T S <43871747+lauvshree@users.noreply.github.com>
2022-11-15 14:54:21 +0530

    Update server.js

--- sample/server.js
+++ sample/server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 http.createServer(function (request, response) {
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end("Hello World!");
+   response.writeHead(200, {'Content-Type': 'application/json'});
+   response.end({"message":"Hello World!","date":new Date());
 }).listen(8080);
 
 console.log('Server running at http://0.0.0.0:8080/');
