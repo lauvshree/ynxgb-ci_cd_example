33d596ea3c270a803ad7d05fb625feeab4217a4f
Lavanya T S <43871747+lauvshree@users.noreply.github.com>
2022-11-15 15:28:43 +0530

    Update server.js

--- sample/server.js
+++ sample/server.js
@@ -1,8 +1,9 @@
 const http = require('http');
 
 http.createServer(function (request, response) {
-   response.writeHead(200, {'Content-Type': 'application/json'});
-   response.end({"message":"Hello World!","date":new Date());
+   response.writeHead(200, {'Content-Type': 'text/string'});
+   let dt = new Date();
+   response.end("This is a new string returned on "+ dt.toDateString()+" at "+dt.toLocaleTimeString('en-US'))
 }).listen(8080);
 
 console.log('Server running at http://0.0.0.0:8080/');
